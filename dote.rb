4c42b4a91b2ef987dd3041eb0642385f1d091e83
mocky70025 <kmc2515@kamiyama.ac.jp>
2025-10-28 11:01:36 +0900

    Update homepage URL and version in dote.rb

--- dote.rb
+++ dote.rb
@@ -1,9 +1,10 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/kmc2400/homebrew-dote"
+  homepage "https://github.com/mocky70025/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
   sha256 "0b9c754b6792fe121675f6fa0a2b3aa2bc1740e41be9c2b5faf72159a112a0e8"
-
+  version “1.0.0”
+  
   def install
     system ENV.cc, "dote.c", "-o", "dote"
     bin.install "dote"
