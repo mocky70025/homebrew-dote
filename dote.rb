599a30bcc8798f1bdb273797dab025ff92507fab
mocky70025 <kmc2515@kamiyama.ac.jp>
2025-10-28 11:23:38 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -1,7 +1,7 @@
 class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/mocky70025/homebrew-dote"
-  url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
+  url "https://raw.githubusercontent.com/mocky70025/homebrew-dote/main/dote.c"
   sha256 "97b6cc9b526fafa5707660842c91c4ab96cf6be1966d67d1abd76a28be2fa2ea"
 
   version "1.0.0"
