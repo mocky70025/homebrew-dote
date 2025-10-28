522d361bc1eaa0b8943fdfcb7d91e0e1c5588a1b
mocky70025 <kmc2515@kamiyama.ac.jp>
2025-10-28 11:50:54 +0900

    Fix formatting of sha256 line in dote.rb

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/mocky70025/homebrew-dote"
   url "https://raw.githubusercontent.com/mocky70025/homebrew-dote/main/dote.c"
-  sha256　"bfa2d5c98434f280ab5e9a061c4f8293422378d790a2c3e579825bcab99faae6"
+  sha256 "bfa2d5c98434f280ab5e9a061c4f8293422378d790a2c3e579825bcab99faae6"
   version "1.0.0"
   
   def install
